15694847160fe1299306d4901448f4cc61f0cd97
Quillon <quillonthegeek@gmail.com>
2023-06-11 00:15:26 +0100

    Build the color mode switch

--- gamehub/src/component/Navbar.tsx
+++ gamehub/src/component/Navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 function Navbar() {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="20px">
       <Image src={logo} boxSize="80px" />
-      <Text>Navbar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 }
