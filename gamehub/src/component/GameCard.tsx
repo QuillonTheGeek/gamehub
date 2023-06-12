5099cd7dd96f3913edf7359c40bfef556b7cc2be
Quillon <quillonthegeek@gmail.com>
2023-06-12 23:48:52 +0100

    display icons

--- gamehub/src/component/GameCard.tsx
+++ gamehub/src/component/GameCard.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformiconList from "./PlatformiconList";
 
-function GameCard() {
-  return <div>GameCar</div>;
+interface Props {
+  game: Game;
+}
+function GameCard({ game }: Props) {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        {/* {game.parent_platforms.map(({ platform }) => (
+          <Text>{platform.name}</Text>
+        ))} */}
+
+        <PlatformiconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </CardBody>
+    </Card>
+  );
 }
 
 export default GameCard;
