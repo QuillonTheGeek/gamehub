0c627af0626847cf156435e0206516e52d4ffee7
Quillon <quillonthegeek@gmail.com>
2023-06-13 13:11:04 +0100

    fetch genre to list

--- gamehub/src/component/GameCard.tsx
+++ gamehub/src/component/GameCard.tsx
@@ -9,13 +9,15 @@ interface Props {
 }
 function GameCard({ game }: Props) {
   return (
-    <Card width=" 300px" borderRadius={10} overflow="hidden">
+    <Card width=" 250px" borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+
         {/* {game.parent_platforms.map(({ platform }) => (
           <Text>{platform.name}</Text>
         ))} */}
+
         <HStack justifyContent="space-between">
           <PlatformiconList
             platforms={game.parent_platforms.map((p) => p.platform)}
---
+++ gamehub/src/hooks/useGenres.ts
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import axios from "axios";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
+
+interface FetchGames {
+  count: number;
+  results: Game[];
+}
+
+interface Genre {
+  id: number,
+  name: string
+}
+interface FetchGenres{
+  count: number;
+  results: Genre[]
+}
+
+function useGenres() {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchGenres>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        if (axios.isCancel(error)) return;
+        setError(error.message);
+        setLoading(false);
+        // setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+  return { genres, error, isLoading };
+}
+
+export default useGenres;
